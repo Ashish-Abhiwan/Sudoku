55b0fd77f4130b140e9e673cd8e81cf4b21a0c15
Ashish-Abhiwan <ashish.abhiwan@gmail.com>
2023-05-26 18:45:54 +0530

    change github link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <footer className="footer">
-      <p>&#169; 2023 source code on <a href="https://github.com/raravi/sudoku">Github</a></p>
+      <p>&#169; 2023 source code on <a href="https://github.com/Ashish-Abhiwan/Sudoku">Github</a></p>
     </footer>
   )
 }
